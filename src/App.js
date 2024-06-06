a9e92fbb52b6f7e1698ccd21e012c19254c17c88
kudinov0222 <155973820+kudinov0222@users.noreply.github.com>
2024-06-06 22:51:03 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Header} from './components/header';
+import {Shop} from './components/shop';
+import {Footer} from './components/footer';
+
+function App() {
+  return (
+    <React.Fragment>
+      < Header />
+      < Shop />
+      < Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return <footer className="page-footer
+    deep-purple accent-1">
+        <div className="footer-copyright">
+            <div ClassName="container">
+                © Чуменко Ярослав {new Date().getFullYear()}
+                <a className="grey-text text-lighten-4 right"
+                href="#!"></a>
+            </div>
+        </div>
+    </footer>
+}
+
+export { Footer };
